47dac1fad353a76a7d158b3df1568d4c70bec12e
Neyran <maxnezerplay@gmail.com>
2026-04-03 03:09:27 +0300

    Update on_command.py

--- events/on_command.py
+++ events/on_command.py
@@ -8,9 +8,24 @@ from dataConfig import LOG_CHANNEL_ID
 @bot.event
 async def on_command(ctx):
     embed = Embed(title=embed_log["title"], color=embed_log["color"])
+    value_map = {
+        "ctx.command": ctx.command,
+        "ctx.author": ctx.author,
+        "ctx.author.id": ctx.author.id,
+        "ctx.message.jump_url": ctx.message.jump_url,
+        "datetime.now().strftime('%Y-%m-%d %H:%M:%S')": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
     for field in embed_log["fields"]:
-        embed.add_field(name=field["name"], value=eval(field["value"]), inline=field["inline"])
-        
+        raw_value = field["value"]
+        value = value_map.get(raw_value, raw_value)
+        embed.add_field(name=field["name"], value=value, inline=field["inline"])
+
     log_channel = bot.get_channel(LOG_CHANNEL_ID)
-    if log_channel:
-        await log_channel.send(embed=embed)
+    if log_channel is None:
+        try:
+            log_channel = await bot.fetch_channel(LOG_CHANNEL_ID)
+        except Exception:
+            return
+
+    await log_channel.send(embed=embed)
